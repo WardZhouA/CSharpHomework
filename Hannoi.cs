770f6dd9743b62a501a6e8422ccb903cf121be0c
WardZhouA <33411000+WardZhouA@users.noreply.github.com>
2018-10-10 16:55:02 +0800

    Add files via upload

---
+++ Hannoi.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hannoi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HannoiCalculate c = new HannoiCalculate();
+            double result = c.calculation(64);
+            Console.WriteLine(result);
+            Console.ReadLine();
+            
+        }
+    }
+    class HannoiCalculate
+    {
+        public double calculation(double a)
+        {
+            if (a==1)
+            {
+                return 1;
+            }
+            else
+            {
+                double sum = 1;
+                sum += calculation(a - 1) * 2;
+                return sum;
+            }
+        }
+    }
+}
+// 1 1
+// 2 2+1
+// 3
