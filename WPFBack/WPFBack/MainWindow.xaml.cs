d6e5682757de4ae55cc3a9a412cd54494167cf25
WardZhouA <33411000+WardZhouA@users.noreply.github.com>
2018-10-10 17:00:43 +0800

    Add files via upload

---
+++ WPFBack/WPFBack/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPFBack
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Action_Click(object sender, RoutedEventArgs e)
+        {
+            int length = 0;
+            string str1;
+            string str2="";
+            length = Tex_before.Text.Length;
+            char[] result = new char[length];
+            str1 = Tex_before.Text;
+            for (int i = length-1; i >=0; i--)
+            {
+                //int j = length-1;
+                str2 += str1.Substring(i, 1);
+                //j--;
+            }
+            Tex_after.Text = str2;
+        }
+
+        private void but_exit_Click(object sender, RoutedEventArgs e)
+        {
+            Environment.Exit(0);
+        }
+    }
+}
---
+++ Wpf_CountString/Wpf_CountString/MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Wpf_CountString
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Action_Click(object sender, RoutedEventArgs e)
+        {
+            string str;
+            str = Tex_main.Text;
+            int let = 0;
+            int num = 0;
+            int oth = 0;
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (str[i]>='0'&&str[i]<='9')
+                {
+                    num++;
+                }
+                else if (str[i] >= 'a' && str[i] <= 'z'|| str[i] >= 'A' && str[i] <= 'Z')
+                {
+                    let++;
+                }
+                else
+                {
+                    oth++;
+                }
+            }
+            Tex_let.Text = Convert.ToString(let);
+            Tex_num.Text = Convert.ToString(num);
+            Tex_oth.Text = Convert.ToString(oth);
+        }
+    }
+}
